e49bb3d8209e68cca39620683be09d67bc3c2c6e
apeironuranium <lera.leroi@gmail.com>
2017-06-13 17:03:06 +0300

    Add files via upload

---
+++ lev.py
@@ -0,0 +1,18 @@
+a = ()
+b = ()
+
+def lev(a,b):
+    if len(a)>len(b):
+        return lev(b,a)
+
+#vagner-fisher
+D = [[0], [0]]
+m = len(a)
+n= len(b)
+for i in [0..m]:
+    for j in [0..n]:
+        D[0,j]
+
+
+
+
