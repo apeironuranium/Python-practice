53ca6a6ebcf8487143841c4315376f0ffd54108d
apeironuranium <lera.leroi@gmail.com>
2017-06-22 16:10:53 +0300

    захватили запятую в elif

--- randLetters.py
+++ randLetters.py
@@ -1,11 +1,12 @@
 import random
+from string import punctuation
 
 def randomize(s):
     l_s = len(s)
-        if (l_s > 2) and (s[-1] not in list (punctuation)):
+    if (l_s > 2) and (s[-1] not in list (punctuation)):
         res = s[0] + "".join(random.sample(s[1:-1], l_s - 2)) + s[-1]
     elif (l_s > 2) and (s[-1] in list (punctuation)):
-        res = s[0] + "".join(random.sample(s[1:-2], l_s - 3)) + s[-2]
+        res = s[0] + "".join(random.sample(s[1:-2], l_s - 3)) + s[-2:]
     else:
         res = s
     return res
