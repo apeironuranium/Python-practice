25ae26738ad62fd1ece6c17195d80ec8d907fb21
apeironuranium <lera.leroi@gmail.com>
2017-06-22 16:08:32 +0300

    изменения в функции randomin

--- randLettersObj.py
+++ randLettersObj.py
@@ -1,35 +1,19 @@
 import random
+from string import punctuation
 
 class Letters(object):
 
     def randomin(self,s):
-        if len(s)>2:
+        if len(s)>2 and (s[-1] not in list(punctuation)):
             return s[0] + "".join(random.sample(s[1:-1], len(s) - 2)) + s[-1]
+        elif len(s) > 2 and (s[-1] in list(punctuation)):
+            return s[0] + "".join(random.sample(s[1:-2], len(s) - 3)) + s[-2:]
         else:
             return s
 
     def out(self, s):
         s_words = s.split()
         print(" ".join([self.randomin(x) for x in s_words]))
-
-
-class Example():
-
-    field = 0
-
-    def __init__(self, field):
-        print(self.field)
-        self.field = field
-        print(self.field)
-
-    def field_increment(self):
-        self.field += 1
-        return self.field
-
-example = Example(1)
-print(example.field_increment())
-print(example.field_increment())
-print(example.field_increment())
 
 text = input("Введите предложение:")
 Letters().out(text)
